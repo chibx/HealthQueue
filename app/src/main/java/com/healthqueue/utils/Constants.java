acd140fd5e751c3aa6b0ea059183b42db00e973a
chibx <111882496+chibx@users.noreply.github.com>
2026-08-07 16:36:32 +0100

    Add http status and message constants for app wide use

--- app/src/main/java/com/healthqueue/utils/Constants.java
+++ app/src/main/java/com/healthqueue/utils/Constants.java
@@ -1,5 +1,7 @@
 package com.healthqueue.utils;
 
+import org.eclipse.jetty.http.HttpStatus;
+
 public class Constants {
 
     public static final String APP_NAME = "HealthQueue";
@@ -9,6 +11,20 @@ public class Constants {
     public static final String CUSTOMER_REFRESH_COOKIE = "cust_refresh_tk";
     public static final String ORG_ACCESS_COOKIE = "org_access_tk";
     public static final String ORG_REFRESH_COOKIE = "org_refresh_tk";
+
+    public static final int STATUS_OK = 200;
+    public static final int STATUS_BAD_REQUEST = 400;
+    public static final int STATUS_UNAUTHORIZED = 401;
+    public static final int STATUS_FORBIDDEN = 403;
+    public static final int STATUS_NOT_FOUND = 404;
+    public static final int STATUS_INTERNAL_ERROR = 500;
+
+    public static final String RESPONSE_OK = "Success";
+    public static final String NOT_FOUND = HttpStatus.getMessage(STATUS_NOT_FOUND);
+    public static final String UNAUTHORIZED = HttpStatus.getMessage(STATUS_UNAUTHORIZED);
+    public static final String BAD_REQUEST = HttpStatus.getMessage(STATUS_BAD_REQUEST);
+    public static final String FORBIDDEN_ERROR = HttpStatus.getMessage(STATUS_FORBIDDEN);
+    public static final String INTERNAL_ERROR = HttpStatus.getMessage(STATUS_INTERNAL_ERROR);
 
     public static final int MIN_PASSWORD_LENGTH = 8;
     public static final int MAX_PASSWORD_LENGTH = 40;
