c91f69a75354e94a2b103f0638a4c8b7f4d17d53
chibx <111882496+chibx@users.noreply.github.com>
2026-08-07 04:43:56 +0100

    Add request structured classes

--- app/src/main/java/com/healthqueue/utils/ServerRequest.java
+++ app/src/main/java/com/healthqueue/utils/ServerRequest.java
@@ -3,11 +3,35 @@ package com.healthqueue.utils;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public class ServerRequest {
+    public static record PatientSignupRequest(
+            @NotBlank String name,
+            @Email String email,
+            @NotBlank @Size(min = 8, max = 15) String phoneNumber,
+            @NotBlank @Size(min = Constants.MIN_PASSWORD_LENGTH, max = Constants.MAX_PASSWORD_LENGTH) String password) {
+    }
+
+    public static record OrganizationSignupRequest(
+            @NotBlank String name,
+            @Email String email,
+            @NotBlank @Size(min = Constants.MIN_PASSWORD_LENGTH, max = Constants.MAX_PASSWORD_LENGTH) String password) {
+    }
+
+    public static record PatientLoginRequest(
+            @Email String email,
+            @NotBlank @Size(min = Constants.MIN_PASSWORD_LENGTH, max = Constants.MAX_PASSWORD_LENGTH) String password) {
+    }
+
+    public static record OrganizationLoginRequest(
+            @Email String email,
+            @NotBlank @Size(min = Constants.MIN_PASSWORD_LENGTH, max = Constants.MAX_PASSWORD_LENGTH) String password) {
+    }
+
     public static record CreateAppointmentRequest(
             @NotBlank String patientName,
             @Email String contactEmail,
-            @Min(0) int branchId) {
+            @Min(0) long branchId) {
     }
 }
